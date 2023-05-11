fdfb2d2d11084bd0074936ec9d542e21d167da41
Peter Körner <pkoerner@seibert-media.net>
2023-05-11 17:09:50 +0200

    generate hash on first visit and use it to shuffle the list of submissions

--- pretalx_halfnarp/views/frontend.py
+++ pretalx_halfnarp/views/frontend.py
@@ -1,3 +1,7 @@
+import random
+import secrets
+from datetime import timedelta
+
 from django.views.generic import TemplateView
 from django_context_decorator import context
 
@@ -9,19 +13,31 @@ from pretalx_halfnarp.views import HASH_COOKIE
 
 class FrontendView(EventPageMixin, TemplateView):
     template_name = "pretalx_halfnarp/frontend.html"
+    halfnarp_hash = None
+
+    def get(self, request, *args, **kwargs):
+        self.halfnarp_hash = self.request.COOKIES.get(HASH_COOKIE, None)
+        print("self.halfnarp_hash", self.halfnarp_hash)
+        if self.halfnarp_hash:
+            return super().get(request, args, kwargs)
+        else:
+            self.halfnarp_hash = secrets.token_hex(64)
+            response = super().get(request, args, kwargs)
+            response.set_cookie(HASH_COOKIE, self.halfnarp_hash, httponly=True, secure=True, samesite="Strict",
+                                max_age=timedelta(days=365).total_seconds())
+            return response
 
     @context
     def submissions(self):
-        return self.request.event.submissions.filter(
-            state__in=[SubmissionStates.ACCEPTED, SubmissionStates.CONFIRMED]
-        )
+        submissions = list(self.request.event.submissions.filter(
+            state__in=[SubmissionStates.ACCEPTED, SubmissionStates.CONFIRMED]))
+        random.Random(self.halfnarp_hash).shuffle(submissions)
+        return submissions
 
     @context
     def preferred_talks(self):
-        halfnarp_hash = self.request.COOKIES.get(HASH_COOKIE, None)
-        if halfnarp_hash:
-            preference = Preference.objects.get(hash=halfnarp_hash)
-            if preference:
-                return list(preference.preferred_submission_ids)
-
-        return []
+        try:
+            preference = Preference.objects.get(hash=self.halfnarp_hash)
+            return list(preference.preferred_submission_ids)
+        except Preference.DoesNotExist:
+            return []
--- pretalx_halfnarp/views/frontend_preferences_api.py
+++ pretalx_halfnarp/views/frontend_preferences_api.py
@@ -1,6 +1,3 @@
-import secrets
-from datetime import timedelta
-
 import jsonschema
 from django.http import JsonResponse
 from rest_framework.permissions import AllowAny
@@ -16,7 +13,7 @@ class FrontendMyPreferencesApi(EventPageMixin, APIView):
     permission_classes = [AllowAny]
 
     def get(self, request, event, *args, **kwargs):
-        halfnarp_hash = self.request.COOKIES.get(HASH_COOKIE, None)
+        halfnarp_hash = request.COOKIES.get(HASH_COOKIE, None)
         if not halfnarp_hash:
             return Response(status=404)
 
@@ -34,17 +31,20 @@ class FrontendMyPreferencesApi(EventPageMixin, APIView):
         except jsonschema.ValidationError as e:
             return Response(status=400, data=str(e))
 
-        halfnarp_hash = self.request.COOKIES.get(HASH_COOKIE, None)
-        if halfnarp_hash:
-            update_preferences(halfnarp_hash, request.data)
-            return Response()
-        else:
-            halfnarp_hash = create_preferences(request.data)
+        halfnarp_hash = request.COOKIES.get(HASH_COOKIE, None)
+        if not halfnarp_hash:
+            return Response(400)
 
-            r = Response()
-            r.set_cookie(HASH_COOKIE, halfnarp_hash, httponly=True, secure=True, samesite="Strict",
-                         max_age=timedelta(days=365).total_seconds())
-            return r
+        try:
+            preference = Preference.objects.get(hash=halfnarp_hash)
+        except Preference.DoesNotExist:
+            preference = Preference()
+            preference.hash = halfnarp_hash
+
+        preference.set_preferred_submission_ids(request.data['preferred_submissions'])
+        preference.save()
+
+        return Response(status=204)
 
 
 def validate_preferences_data(request_data):
@@ -61,18 +61,3 @@ def validate_preferences_data(request_data):
         "required": ["preferred_submissions"]
     }
     jsonschema.validate(request_data, schema)
-
-
-def update_preferences(halfnarp_hash, request_data):
-    preference = Preference.objects.get(hash=halfnarp_hash)
-    preference.set_preferred_submission_ids(request_data['preferred_submissions'])
-    preference.save()
-
-
-def create_preferences(request_data):
-    halfnarp_hash = secrets.token_hex(64)
-    preference = Preference()
-    preference.hash = halfnarp_hash
-    preference.set_preferred_submission_ids(request_data['preferred_submissions'])
-    preference.save()
-    return halfnarp_hash
