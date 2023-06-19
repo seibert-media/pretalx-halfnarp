8728a25c5fdb7d118b67826dec89e932e0f6917d
Franziska Kunsmann <fkunsmann@seibert-media.net>
2023-06-19 10:39:52 +0200

    use django.urls.re_path() instead of django.conf.urls.url()

--- pretalx_halfnarp/urls.py
+++ pretalx_halfnarp/urls.py
@@ -1,20 +1,20 @@
-from django.conf.urls import url
+from django.urls import re_path
 from pretalx.event.models.event import SLUG_CHARS
 
 from .views import frontend, frontend_preferences_api, organiser
 
 urlpatterns = [
-    url(
+    re_path(
         f"^(?P<event>[{SLUG_CHARS}]+)/p/halfnarp/$",
         frontend.FrontendView.as_view(),
         name="frontend",
     ),
-    url(
+    re_path(
         f"^(?P<event>[{SLUG_CHARS}]+)/p/halfnarp/my-preferences/$",
         frontend_preferences_api.FrontendMyPreferencesApi.as_view(),
         name="frontend_my_preferences_api",
     ),
-    url(
+    re_path(
         f"^orga/event/(?P<event>[{SLUG_CHARS}]+)/p/halfnarp/$",
         organiser.OrganiserView.as_view(),
         name="organiser",
